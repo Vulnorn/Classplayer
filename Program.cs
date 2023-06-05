ea3a55ec12b4033dcab99d75db0e454e637169ec
Vulnorn <mixadrbk31@gmail.com>
2023-06-05 12:11:33 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Player", 1);
+
+            player.ShowStatsPlayer();
+        }
+    }
+
+    class Player
+    {
+        public string Name;
+        public int Level;
+
+        public Player(string name, int level) 
+        { 
+            Name = name;
+            Level = level;
+        }
+
+        public void ShowStatsPlayer() 
+        {
+            Console.WriteLine($"Имя Игрока {Name}, Уровень Игрока - {Level}");
+            Console.ReadKey();
+        }
+    }
+}
